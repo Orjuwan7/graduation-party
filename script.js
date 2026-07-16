d3a22aa1cfdd49d88311def8b829d9cf48bbc56e
Orjuwan7 <arjmm448@gmail.com>
2026-07-16 13:57:07 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-const openBtn = document.getElementById("openBtn");
-const cover = document.getElementById("cover");
-const invitation = document.getElementById("invitation");
-
-openBtn.addEventListener("click", () => {
-
-    cover.style.opacity = "0";
-    cover.style.transition = "0.8s";
-
-    setTimeout(() => {
-        cover.style.display = "none";
-        invitation.classList.remove("hidden");
-
-        window.scrollTo({
-            top: 0,
-            behavior: "smooth"
-        });
-
-    }, 700);
-
-});
+function showDetails() {
+    document.getElementById("details").style.display = "block";
+}
